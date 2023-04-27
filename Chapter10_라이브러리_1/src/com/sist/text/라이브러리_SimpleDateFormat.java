e4159d8f8eab15f6724413ff07183dc93cda3b81
yoo-hyunseung <hsung122@naver.com>
2023-04-27 18:01:32 +0900

    라이브러리

---
+++ Chapter10_라이브러리_1/src/com/sist/text/라이브러리_SimpleDateFormat.java
@@ -0,0 +1,42 @@
+package com.sist.text;
+/*
+ *    java.text => 출력 형태를 만들어서 사용(변경)
+ *    --------- 
+ *      SimpleDateFormat : 날짜 변경
+ *      DecimalFormat : 숫자 변환
+ *      -------------- 가격출력 10,000원
+ *      MessageFormat : 데이터베이스 -> INSERT, UPDATE
+ *      -------------
+ *      String name = "" , sex="",addr="",tel="";
+ *      int age = 10;
+ *      
+ *      String sql = "INSERT INTO member VALUES("
+ *                    +"'"+name+"','"+sex+"','"+addr+"','"+tel+"',...)
+ *      String
+ */
+import java.util.*;
+import java.text.*;
+
+public class 라이브러리_SimpleDateFormat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		java.util.Date date = new java.util.Date();
+//		Date date = new Date(); // 충돌 java.util.Date; 별도로 선언해야 위아래 중 하나 선택
+//		System.out.println(date.toString());
+//		System.out.println(date);
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+//		System.out.println(sdf.format(date));
+		/*
+		 *    yyyy년도 = rrrr
+		 *    MM 월  ==> mm
+		 *    dd 일  ==> dd
+		 *    hh 시간 ==> hh
+		 *    mm 분  ==> mi
+		 *    ss 초 ==> ss
+		 *    
+		 */
+		
+	}
+
+}
